1280e2779d3893800683947c723009fbc7088c9d
André <mrawesomeferreira@gmail.com>
2025-03-11 21:31:16 +0000

    adds lesson-1.js

---
+++ lesson-1.js
@@ -0,0 +1,58 @@
+// Statement
+/* console.log("Hello world");
+console.log("Again"); */
+
+// Line comment
+/* Multi 
+Line
+Comment
+
+alt + shift + a
+
+ */
+
+// Data Types
+
+// Strings - Used to represent text
+console.log("Double Quotes");
+console.log("Single Quote");
+console.log(`Backticks`);
+
+console.log('My favorite book is "Lord of the rings"');
+
+// Numbers - Represent numbers, whole or decimals
+console.log("2025");
+console.log(2025);
+console.log(5.5);
+
+// Mathematical operations
+// Sum
+console.log(20 + 25);
+console.log("20 + 25");
+console.log("20" + "25");
+console.log(25 - 5);
+console.log(25 * 5);
+console.log(25 / 5);
+// PEMDAS - Parenthesis, Exponential, Multiplication, Division, Adittion, Subtraction
+console.log(100 - (20 * 4) / 5);
+
+// Type Coercion
+console.log("I am " + 25);
+console.log("I am " + "25");
+
+// NaN - Not a number - when we have an error in a mathematical operation
+console.log("I am " + 2025 - 1994 + "years old");
+// "I am 2025"
+// "I am 2025" - 1994 - Nan
+// Nanyearsold
+
+//Fixed
+console.log("I am " + (2025 - 1994) + " years old");
+// "I am " + 31 + "years old"
+// "I am 31 " + "years old"
+// "I am 31 years old"
+
+// typeof - helps us check the data type of something
+console.log(typeof 25);
+console.log(typeof "25");
+console.log(typeof NaN);
