da2367ca76343864d78d500014a7de1219b5051b
André <mrawesomeferreira@gmail.com>
2025-04-08 18:57:28 +0100

    adds challenge

---
+++ challenge.js
@@ -0,0 +1,54 @@
+// ---------- 1 - Variables
+
+// Create a variable called 'myName' and assign your first name to it
+// log the value to the console
+
+// ---------- 2 - Strings and numbers
+// Using the variables below, create a new variable that holds the value "I am 99 years old"
+
+let iAm = "I am";
+let age = 99;
+let yOld = "years old";
+
+// ---------- 3 - If statements
+// Create an if statement that checks if a number is even or odd. If it's even, log "Even" to the console, otherwise log "Odd"
+// you can use the below variables for testing
+
+let testingNumber1 = 171284;
+let testingNumber2 = 843;
+
+// ---------- 4 - Functions
+// Create a function called 'greet' that receives a name as an argument. When invoked, the function should log to the console "Hello [name]!"
+// Test it with your own name
+
+// ---------- 5 - Arrays
+// The following array contains things that should not go in a fruit salad
+
+let fruitSalad = ["Salt", "Mango", "Apple", "Orange", "Rice"];
+
+// 5.1 - Remove the last element from the array
+
+// 5.2 - Remove the first element of the array
+
+// 5.3 - Add to the array two more fruits, one at the end and one at the beginning
+
+// ---------- 6 - Objects
+// Create an object called 'product'. The object should have the properties:
+//  'name' (a string)
+//  'price' (a number)
+//  'available' (a boolean)
+//  'seller' (an object with the properties 'name' (a string) and city (a string))
+// Assign any value you want to those properties
+
+// 6.1 Log to the console the name of the seller
+
+// ---------- 7 - Loops
+// Write a for loop that logs to the console numbers from 0 to 20.
+// BONUS: On the numbers 10 and 20, instead of logging 10 and 20, log "Ten" and "Twenty"
+
+// ---------- 8 - Bonus
+// 8.1 - Using the array below, create a new filtered array called 'above100' with only the numbers above 100
+
+let myNumbers = [8, 12, 982, 1923, 918, 78, 0, 111, 67];
+
+// 8.2 - With the new 'above100' array, create a new array 'doubled' which has all of the previous values but doubled
